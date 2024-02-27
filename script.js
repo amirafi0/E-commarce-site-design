052cd4b508843d8e7eae830af25a475cab862fd9
Ami rafi <kh9961734@gmail.com>
2024-02-28 00:14:30 +0600

    product card design

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let images = ['../images/slider1.jpg','../images/slider2.jpg','../images/slider3.jpg'];
-index = 0;
-time = 3000;
+function imgSlide(){
+    let url = "img.json"
+    fetch(url)
+     .then(res => res.json())
+     .then(images => {
+
+
+        index = 0;
+        time = 3000;
 
 function slider(){
     let slider = document.querySelector('.slider');
-    let code = `<img src='${images[index]}'/>`
+    let code = `<img src='${images[index].img}'/>`
 
     slider.innerHTML= code;
 
@@ -17,4 +23,55 @@ function slider(){
 
 setInterval(slider, time);
 
-slider()
+
+     })
+}
+
+imgSlide()
+
+
+// product container
+
+
+let prdturl = "product.json"
+fetch(prdturl)
+ .then(res => res.json())
+ .then(res =>{
+     card(res)
+ });
+
+ function card(prdt){
+    let prdtCard = document.querySelector('.prdt-card');
+
+    let user = ' '
+
+    prdt.forEach(value => {
+        
+       
+
+       user += html(value)
+    })
+    prdtCard.innerHTML = user ;
+ }
+
+
+ function html(prdt){
+ const {img, title, price } = prdt;
+
+
+     const code  = `
+
+        <div>
+            <img src='${img}'/>
+            <p>${title.length > 22 ? title.slice(0, 22)+ '...' : title}</p>
+            <h2>${price}</h2>
+            
+
+        </div>
+     
+     `;
+
+     return code;
+ }
+
+
